6c56c827725bd4ffb972f2731fda5a54bbfc4dd2
campeon91 <fahmi.gh@gmail.com>
2018-04-08 22:57:59 +0200

    Add plog logging library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
+#include <plog/Log.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
 
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello cim !!" << std::endl;
+    plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+    plog::init(plog::verbose, &consoleAppender);
+    LOG_DEBUG << "Hello cim";
     return 0;
 }
